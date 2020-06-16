3529ca242ec67c2e5e320dcaf948c3c5002380e3
sleyzerzon <excelsior.software@yahoo.com>
2020-06-16 11:19:22 -0400

    adding the db

--- src/main/java/com/excelsiorsoft/PersonController.java
+++ src/main/java/com/excelsiorsoft/PersonController.java
@@ -3,7 +3,11 @@
  */
 package com.excelsiorsoft;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -14,10 +18,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class PersonController {
 
+	@Autowired
+	private  GreetingRepository greetingRepository;
+	
 	@RequestMapping("/")
 	@ResponseBody
 	String home() {
 		return "Hello World!";
+	}
+	
+	@GetMapping("/greetings")
+	Iterable<Greeting> greetings(){
+		return greetingRepository.findAll();
 	}
 
 }
--- src/main/java/com/excelsiorsoft/VeeaInterviewApplication.java
+++ src/main/java/com/excelsiorsoft/VeeaInterviewApplication.java
@@ -1,9 +1,16 @@
 package com.excelsiorsoft;
 
-import org.springframework.boot.ApplicationArguments;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.repository.CrudRepository;
+
+import lombok.Data;
 
 @SpringBootApplication
 public class VeeaInterviewApplication {
@@ -13,5 +20,30 @@ public class VeeaInterviewApplication {
 		SpringApplication.run(VeeaInterviewApplication.class, args);
 	}
 	
-
+	@Bean
+	ApplicationRunner applicationRunner(GreetingRepository greetingRepository) {
+		return args -> {
+			greetingRepository.save(new Greeting("Hello"));
+			greetingRepository.save(new Greeting("Hi"));
+		};
+	}
+	
 }
+
+@Entity
+@Data
+class Greeting{
+	
+	public Greeting(String greeting) {
+		this.message = greeting;
+	}
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String message;
+	
+}
+
+interface GreetingRepository extends CrudRepository<Greeting, Long>{}
