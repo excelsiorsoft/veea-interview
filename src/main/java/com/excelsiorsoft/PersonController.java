39dedd28992ac98b325b4b48397b09f10e549d31
sleyzerzon <excelsior.software@yahoo.com>
2020-06-16 11:37:29 -0400

    adding the db

--- src/main/java/com/excelsiorsoft/PersonController.java
+++ src/main/java/com/excelsiorsoft/PersonController.java
@@ -3,19 +3,17 @@
  */
 package com.excelsiorsoft;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Simeon
  *
  */
-@Controller
+@RestController
 public class PersonController {
 
 	@Autowired
