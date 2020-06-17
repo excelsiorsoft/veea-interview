14306a59d02b2542506b7c32492ac25c70e46bac
sleyzerzon <excelsior.software@yahoo.com>
2020-06-17 12:11:55 -0400

    jsonpath

--- src/main/java/com/excelsiorsoft/PlacesService.java
+++ src/main/java/com/excelsiorsoft/PlacesService.java
@@ -3,7 +3,9 @@
  */
 package com.excelsiorsoft;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -35,10 +37,20 @@ public class PlacesService {
 	@Value("${foursquare.version}")
 	private String version;
 	
+	@SuppressWarnings("serial")
 	public Places obtainVenues() {
+		
 		RestTemplate restTemplate = new RestTemplate();
-		String venuesSearchUrl = "https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id= {clientId}&client_secret={clientSecret}&v={version}";
-		ResponseEntity<String> response = restTemplate.getForEntity(venuesSearchUrl, String.class);
+		
+		String venuesSearchUrl = "https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id= {ci}&client_secret={cs}&v={v}";
+		
+		Map<String, String> uriVariables = new HashMap<String, String>() {{
+			put("ci", clientId);    
+			put("cs", clientSecret);
+			put("v", version);      
+		}};
+		
+		ResponseEntity<String> response = restTemplate.getForEntity(venuesSearchUrl, String.class, uriVariables);
 		
 		log.debug("FourSquare response body: {}", response.getBody());
 		
