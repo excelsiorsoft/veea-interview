33cc846f388fe428f8eebbb1804144fcc513ac41
sleyzerzon <excelsior.software@yahoo.com>
2020-06-17 11:53:01 -0400

    jsonpath

--- src/main/java/com/excelsiorsoft/Places.java
+++ src/main/java/com/excelsiorsoft/Places.java
@@ -8,18 +8,16 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 /**
+ * Representation of the vicinity venues obtain (as in this example from FourSquare) or elsewhere
+ * 
  * @author Simeon
  *
  */
 @Data
-//@AllArgsConstructor
-//@NoArgsConstructor
 @JsonInclude(Include.NON_NULL)
 @Builder
 public class Places {
--- src/main/java/com/excelsiorsoft/PlacesService.java
+++ src/main/java/com/excelsiorsoft/PlacesService.java
@@ -5,12 +5,12 @@ package com.excelsiorsoft;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.excelsiorsoft.Places.PlacesBuilder;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.JsonPath;
 
 import lombok.extern.log4j.Log4j2;
@@ -23,25 +23,25 @@ import lombok.extern.log4j.Log4j2;
 @Service
 public class PlacesService {
 	
-	private static final ObjectMapper MAPPER = new ObjectMapper();
+	@Value("foursquare.venues.query")
+	private String venuesQuery;
 	
 	public Places obtainVenues() {
 		RestTemplate restTemplate = new RestTemplate();
 		String venuesSearchUrl = "https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=4VMTK3PJUNCYS2WBR3LHOUIZOVULPA4QI4LQZD0CBV24VF3J&client_secret=B0DUHZAWTG4FKQGQLUGWI2FCXZUT1HF3CPFHM3TU5LLPZB4L&v=20200616";
 		ResponseEntity<String> response = restTemplate.getForEntity(venuesSearchUrl, String.class);
 		
-		log.info("FourSquare response: {}", response);
-		log.info("FourSquare response body: {}", response.getBody());
+		log.debug("FourSquare response body: {}", response.getBody());
 		
 		String json = response.getBody();
 
-		List<String> venues = JsonPath.read(json, "response.venues[*].name");	
+		List<String> venues = JsonPath.read(json, venuesQuery);	
 		
-		log.info("JsonPath list of venues: {}", venues);
+		log.debug("JsonPath list of venues: {}", venues);
 		
 		Places result = new PlacesBuilder().places(venues).build();
 		
-		log.info("Built Places dto: {}", result);
+		log.debug("Built Places dto: {}", result);
 		
 		return result;
 		
